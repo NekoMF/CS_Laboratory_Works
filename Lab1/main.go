7833071f2605272217cd9290e4a18ecfb2fbe42c
NekoMF <nicu.savva@gmail.com>
2022-09-29 21:18:41 +0300

    Add Vigenere Encryption

--- Lab1/main.go
+++ Lab1/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/NekoMF/CS_Laboratory_Works/playfair"
+	"github.com/NekoMF/CS_Laboratory_Works/useful"
+	"github.com/NekoMF/CS_Laboratory_Works/vigenere"
 )
 
 func main() {
 
-	str := "I n   st r u m e      nt     s"
+	str := useful.CutBlankSpaces("I n   st r u m e      nt     s")
+
 	key := "monarchy"
 
 	log.Println(" This is the string before encryption : ", str)
 
 	playfairEncrypted := playfair.Encrypt(key, str)
-	log.Println(" This is the string after playfair encryption : ", playfairEncrypted)
+	log.Println("\n This is the string after playfair encryption : ", strings.ToLower(playfairEncrypted))
+
+	playfairEncrypted = vigenere.Encrypt(key, str)
+	log.Println(" This is the string after vigenere encryption : ", strings.ToLower(playfairEncrypted))
 
 }
