e85395c9aa8dee93c1fd7c7aff14b7761aa230c6
NekoMF <nicu.savva@gmail.com>
2022-09-29 22:51:49 +0300

    Add Caesar Encryption

--- Lab1/caesar/caesar.go
+++ Lab1/caesar/caesar.go
@@ -1,7 +1,50 @@
 package caesar
 
-import "log"
+import (
+	"math/rand"
+	"strings"
+)
 
-func Blea() {
-	log.Printf("go")
+func Encrypt(alphabet string, shift int, text string) string {
+	var result strings.Builder
+	for _, c := range text {
+		if c == ' ' {
+			result.WriteByte(' ')
+			continue
+		}
+
+		index := strings.Index(alphabet, string(c))
+		if index == -1 {
+			panic("invalid character")
+		}
+
+		result.WriteByte(alphabet[(index+shift)%len(alphabet)])
+	}
+	return result.String()
+}
+
+func Decrypt(alphabet string, shift int, text string) string {
+	return Encrypt(alphabet, len(alphabet)-shift, text)
+}
+
+func PermEncrypt(alphabet string, seed int64, shift int, text string) string {
+
+	newAlphabet := permute(alphabet, seed)
+
+	return Encrypt(string(newAlphabet), shift, text)
+}
+
+func PermDecrypt(alphabet string, seed int64, shift int, text string) string {
+
+	newAlphabet := permute(alphabet, seed)
+
+	return Decrypt(string(newAlphabet), shift, text)
+}
+
+func permute(alphabet string, seed int64) string {
+	a := []rune(alphabet)
+
+	rand.Seed(seed)
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+	return string(a)
 }
--- Lab1/main.go
+++ Lab1/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/NekoMF/CS_Laboratory_Works/caesar"
 	"github.com/NekoMF/CS_Laboratory_Works/playfair"
 	"github.com/NekoMF/CS_Laboratory_Works/useful"
 	"github.com/NekoMF/CS_Laboratory_Works/vigenere"
@@ -11,16 +12,33 @@ import (
 
 func main() {
 
-	str := useful.CutBlankSpaces("I n   st r u m e      nt     s")
-
+	alfabet := "abcdefghijklmnopqrstuvwxyz"
+	var s int64 = 69420
+	permutation := 10
+	str := "I   nst  rum     ents"
 	key := "monarchy"
+
+	str = strings.ToLower(useful.CutBlankSpaces(str))
 
 	log.Println(" This is the string before encryption : ", str)
 
+	caesarEncrypted := caesar.PermEncrypt(alfabet, s, permutation, str)
+	log.Println("\nThis is the string after CAESAR encryption :  ", caesarEncrypted)
+
+	caesarDecrypted := caesar.PermDecrypt(alfabet, s, permutation, caesarEncrypted)
+	log.Println("\nDecrypted : ", caesarDecrypted)
+
 	playfairEncrypted := playfair.Encrypt(key, str)
 	log.Println("\n This is the string after playfair encryption : ", strings.ToLower(playfairEncrypted))
+	playfairDecrypted := playfair.Decrypt(key, playfairEncrypted)
+	log.Println("\n Decrypted : ", strings.ToLower(playfairDecrypted))
 
-	playfairEncrypted = vigenere.Encrypt(key, str)
-	log.Println(" This is the string after vigenere encryption : ", strings.ToLower(playfairEncrypted))
+	vigenereEncrypted := vigenere.Encrypt(key, str)
+	log.Println("\n This is the string after vigenere encryption : ", strings.ToLower(vigenereEncrypted))
+	vigenereDecrypted := vigenere.Decrypt(key, vigenereEncrypted)
+	log.Println("\n Decrypted : ", strings.ToLower(vigenereDecrypted))
+
+	// caesarEncrypted := caesar.PermEncrypt(alfabet, s, permutations, str)
+	// log.Println("\n This is the string after caesar permutation encryption : ", caesarEncrypted)
 
 }
